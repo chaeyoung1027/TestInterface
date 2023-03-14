a1672cb1bee527c952431df984a11d8c849219d9
Chaeyoung Lim <rnldyal1027@naver.com>
2023-03-14 15:19:11 +0900

    인터페이스 수정

--- src/main/java/kr/hs/study/Beans/TestBean1.java
+++ src/main/java/kr/hs/study/Beans/TestBean1.java
@@ -1,4 +1,8 @@
 package kr.hs.study.Beans;
 
 public class TestBean1 implements TestBean{
+    @Override
+    public void test() {
+
+    }
 }
--- src/main/java/kr/hs/study/Beans/TestBean2.java
+++ src/main/java/kr/hs/study/Beans/TestBean2.java
@@ -1,4 +1,8 @@
 package kr.hs.study.Beans;
 
 public class TestBean2 implements TestBean{
+    @Override
+    public void test() {
+
+    }
 }
